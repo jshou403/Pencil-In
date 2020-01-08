4acc3b9132d103c94488121c6bbc0c7911914280
Jacalyn Shou <JSHOU403@GMAIL.COM>
2020-01-08 12:08:12 -0500

    Resolved weird merge conflict.

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -2,22 +2,14 @@ const db = require("../models");
 
 module.exports = {
     // lsit of functions that will be exported when we require our book model to perform crud operations 
-<<<<<<< HEAD
-    // findAll: function(req, res) {
-    //     db.Student
-    //       .find(req.query)
-    //       .then(dbStudent => res.json(dbStudent))
-    //       .catch(err => res.status(422).json(err));
-    //   }
 
-=======
     findAll: function(req, res) {
         db.Student
           .find({})
           .then(dbStudent => res.json(dbStudent))
           .catch(err => res.status(422).json(err));
       },
->>>>>>> 64a105ef65aa4eaaa9fd1670088c0dd8d9979be7
+
     //   findById: function(req, res) {
     //     db.User
     //       .findById(req.params.id)
