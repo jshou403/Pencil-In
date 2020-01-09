b6a5cc181af44bb3581a415fb73c648c64772a39
Aliesa Jackson <aliesaj2010@yahoo.com>
2020-01-08 22:12:19 -0500

    updated font color for username in nav

--- client/src/components/StudentList.js
+++ client/src/components/StudentList.js
@@ -3,12 +3,9 @@ import React from 'react';
 //pass in student prop from teacherhome.js
 function StudentList(student) {
 	return (
-		// <div className="row mt-6">
-			// <div className="col-3 px-md-5 mt-3">
-				<div className="col mt-3">
-				{student.firstName} {student.lastName}
-			</div>
-		// </div>
+		<div className="col mt-3">
+			{student.firstName} {student.lastName}
+		</div>
 	);
 }
 export default StudentList;
